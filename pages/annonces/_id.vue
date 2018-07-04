88c7749e8de8bdf7cb7692ca959706cab9d2fed3
Antoine Guillemoto <antoine.guillemoto@rueducommerce.com>
2018-07-04 15:56:47 +0200

    feat() add mock instance annonce

--- pages/annonces/_id.vue
+++ pages/annonces/_id.vue
@@ -4,22 +4,31 @@
         <span class="last-post"></span>   
         <div class="search-result">         
           <h1>{{ annonce.sport.name }} - {{ annonce.name }}</h1>
-          <h2>{{ annonce.creator.username }} </h2>  
+          <h2>{{ annonce.creator.username }}</h2>
         </div>
+        <Instance :instance="instance" v-for="instance in echeances" :key="instance._id" />
       </aside>
-      <aside class="cs-content cs-side-content">
-        <div class="cs-banner">           
-        </div>
-      </aside>     
     </div>
    
 </template>
 
 <script>
+  import Instance from '~/components/Instance.vue'
+
   export default {
+    components: {
+      Instance
+    },
     computed: {
       annonce () {
         return this.$store.state.annonce
+      },
+      echeances () {
+        return [
+          { date: new Date(), subscribers: [this.annonce.creator], confirmed: true, cancel: false, registered: true, nbPlaces: 10 },
+          { date: new Date(), subscribers: [this.annonce.creator, {username: 'Antoine'}, {username: 'Pierre'}], confirmed: false, cancel: false, nbPlaces: 10 },
+          { date: new Date(), subscribers: [this.annonce.creator, {username: 'Pierre'}, {username: 'Antoine'}], confirmed: false, cancel: true, nbPlaces: 10 }
+        ]
       }
     },
     asyncData (context) {
