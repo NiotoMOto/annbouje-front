a0ea97dec9fa7346a26cb4e6241ee7dcd6ce7e23
Pierre Wadlaire FERON <wadjoaf@gmail.com>
2018-07-30 00:08:07 +0200

    feat(announc) start styling announce page details

--- components/Annonce.vue
+++ components/Annonce.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="annonce">
-      <p>{{ annonce.date | formatDate }}</p>
-      <p>
-        {{ annonce.subscribers.length }} {{ $t('labels.subscribers') }} / {{ annonce.places }} {{ $t('labels.place') }}
+    <div class="annonce-details">
+      <p class="ann-title">Urban soccer - Porte d'Aubervilliers</p>
+      <p class="ann-date">Prévu pour le : <span class="date">{{ annonce.date | formatDate }}</span></p>
+      <p class="ann-places-available">
+        <!-- {{ $t('labels.subscribers') }} -->
+        Il reste <span class="places-available">{{ annonce.subscribers.length }} {{ $t('labels.place') }} sur {{ annonce.places }} </span> 
       </p>
-      <StatusInstance :confirmed="annonce.confirmed" :cancel="annonce.cancel" />
-      {{ annonce.registered ? 'REGISTERED': '' }}
-      <div v-for="subscriber of annonce.subscribers" :key="subscriber.username">
-        {{ subscriber.username }}
+      <div class="ann-suscribers">Voir la liste des inscrits
+        {{ annonce.registered ? 'REGISTERED': '' }}
+        <p v-for="subscriber of annonce.subscribers" :key="subscriber.username">
+          {{ subscriber.username }}
+        </p>
       </div>
+      <!-- <button>
+        je m'inscris
+      </button> -->
+      <!-- <StatusInstance :confirmed="annonce.confirmed" :cancel="annonce.cancel" /> -->     
       <div v-if="!annonce.registered"><button>{{ $t('button.subscribe') }}</button></div>
     </div>
 </template>
@@ -22,8 +29,28 @@
 </script>
 
 <style lang="scss">
-  .annonce {
-    padding: 10px;
+  .annonce-details {
+    color: #666;
+    float: left;
+    width: 40%;
+  }
+
+  .ann-title {
+    font-size: 18px;
+    color: #41b883;
+    font-weight: 500;
+    margin-bottom: 8px;
+  }
+  .ann-date {
+    .date {
+      color: #41b883;
+    }
+  }
+  .places-available {
+    color: #41b883;
+  }
+  .ann-suscribers {
+    margin-top: 8px;
   }
 </style>
 
--- pages/annonces/_id.vue
+++ pages/annonces/_id.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="cs-content-wrapper">       
       <aside class="cs-content cs-main-content">  
-        <span class="last-post"></span>   
-        <div>         
-          <!-- <h1>{{ annonce.annonce.name }} - {{ annonce.name }}</h1>
-          <h2>{{ annonce.annonce.username }}</h2> -->
-        </div>
-        <div>
+        <!-- <span class="last-post"></span>    -->
+        <!-- <div>         
+          <h1>{{ annonce.annonce.name }} - {{ annonce.name }}</h1>
+          <h2>{{ annonce.annonce.username }}</h2>
+        </div> -->
+        <Annonce :annonce="annonce" :key="annonce._id" />
+        <div class="map-wrapper">
+          <p class="localisation-bloc">
+           <span class="loc-label">Les coordonnées :</span>  
+           <span class="loc-address">2 rue de Paris 93564 Aubervilliers</span> 
+          </p>
           <GmapMap
             :center="{lat:48.0300024, lng:2.739625}"
             :zoom="14"
             map-type-id="terrain"
-            style="width: 500px; height: 300px"
+            style="width: 100%; height: 300px"
           >
             <GmapMarker
               :position="{lat:48.0300024, lng:2.739625}"
             />
           </GmapMap>
-        </div>
-        <div></div>
-        <Annonce :annonce="annonce" :key="annonce._id" />
+        </div>        
       </aside>
     </div>
    
@@ -52,5 +55,27 @@
 </script>
 
 <style lang="scss">
+.cs-main-content {
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 90px;
+}
+.map-wrapper {
+  width: 60%;
+  float: left;
+  margin-bottom: 32px;
+}
+.localisation-bloc {
+  color: #666;  
+  width: 100%;
+  margin-bottom: 8px;
+  .loc-label {
+    color: #41b883;
+    font-weight: 500;
+    display: inline-block;
+    width: 100%;
+  } 
+
+}
 </style>
 
