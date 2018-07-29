a3ca318efbc3d7b2ca7706e075db359d1ded0d33
Nioto <antoine.guillemoto@gmail.com>
2018-07-29 20:35:36 +0000

    feat(app) working vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ module.exports = {
 
   modules: [
     '@nuxtjs/apollo',
+    '@nuxtjs/vuetify',
     [
       '@nuxtjs/axios',
       {
@@ -59,7 +60,19 @@ module.exports = {
         // required  
         httpEndpoint: config.apolloUrl,
         websocketsOnly: false // Optional
-      },
+      }
+    }
+  },
+  vuetify: {
+    // Vuetify options
+    theme: {
+      primary: '#1976D2',
+      secondary: '#424242',
+      accent: '#82B1FF',
+      error: '#FF5252',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FFC107'
     }
   },
   router: {
